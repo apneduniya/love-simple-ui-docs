d5b013980d117bf72ba79c21f71edfe870a803e5
thekavikumar <kavikumarceo@gmail.com>
2023-05-19 20:17:45 +0530

    gssoc

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,16 +12,16 @@ const config: DocsThemeConfig = {
     }
   },
   faviconGlyph: "✨",
-  docsRepositoryBase: "https://github.com/thekavikumar/simple-ui-devstuff",
-  logo: <h1 className="logo">Simple UI</h1>,
+  docsRepositoryBase: "https://github.com/thekavikumar/love-simple-ui",
+  logo: <h1 className="logo">Love Simple UI</h1>,
   project: {
-    link: "https://github.com/thekavikumar/simple-ui-devstuff",
+    link: "https://github.com/thekavikumar/love-simple-ui",
   },
   chat: {
     link: "https://discord.gg/kBefU7D76n",
   },
   footer: {
-    text: "© 2022 Simple UI. Made with ❤️ by Kavikumar.",
+    text: "© 2023 Love Simple UI. Made with ❤️ by Kavikumar.",
   },
 };
 
